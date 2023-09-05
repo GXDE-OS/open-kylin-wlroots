dbd5f200492fde06beae65f1f0ea6fb6e0796c18
Kirill Primak <vyivel@eclair.cafe>
2023-09-05 16:14:25 +0300

    xdg-{shell,decoration}: improve events
    This commit changes the behavior of `new_*` signals to better match
    their names (see https://gitlab.freedesktop.org/wlroots/wlroots/-/issues/3608).
    wlr_xdg_shell.events.new_surface is now emitted when an xdg_surface is
    created, and wlr_xdg_shell.events.new_{toplevel,popup} events are
    introduced to get notified when an xdg_{toplevel,popup} is created.
    Same applies to
    `wlr_xdg_decoration_manager_v1.events.new_toplevel_decoration`. As a
    result, wlr_xdg_surface.added and wlr_xdg_toplevel_decoration_v1.added
    are removed, as we no longer need to track whether the corresponding
    event was emitted.
    
    Additionally, this commit changes the behavior of
    wlr_xdg_surface.events.destroy: it is now emitted when the xdg_surface
    is destroyed, as the name suggests.
    wlr_xdg_{toplevel,popup}.events.destroy events are added to get
    notified when an xdg_{toplevel,popup} is destroyed.

--- types/xdg_shell/wlr_xdg_popup.c
+++ types/xdg_shell/wlr_xdg_popup.c
@@ -417,6 +417,7 @@ void create_xdg_popup(struct wlr_xdg_surface *surface, struct wlr_xdg_surface *p
 		&positioner->rules, &surface->popup->scheduled.geometry);
 	surface->popup->scheduled.rules = positioner->rules;
 
+	wl_signal_init(&surface->popup->events.destroy);
 	wl_signal_init(&surface->popup->events.reposition);
 
 	if (parent) {
@@ -428,6 +429,7 @@ void create_xdg_popup(struct wlr_xdg_surface *surface, struct wlr_xdg_surface *p
 
 	set_xdg_surface_role_object(surface, surface->popup->resource);
 
+	wl_signal_emit_mutable(&surface->client->shell->events.new_popup, surface->popup);
 	if (parent) {
 		wl_signal_emit_mutable(&parent->events.new_popup, surface->popup);
 	}
@@ -465,14 +467,9 @@ void destroy_xdg_popup(struct wlr_xdg_popup *popup) {
 	wlr_surface_unmap(popup->base->surface);
 	reset_xdg_popup(popup);
 
-	// TODO: improve events
-	if (popup->base->added) {
-		wl_signal_emit_mutable(&popup->base->events.destroy, NULL);
-		popup->base->added = false;
-	}
+	wl_signal_emit_mutable(&popup->events.destroy, NULL);
 
 	popup->base->popup = NULL;
-
 	wl_list_remove(&popup->link);
 	wl_resource_set_user_data(popup->resource, NULL);
 	free(popup);
