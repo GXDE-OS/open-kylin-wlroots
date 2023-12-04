dbf211b65aa56801aeb51247c1b12ba70edefd58
Simon Ser <contact@emersion.fr>
2023-12-04 19:32:44 +0100

    compositor: drop surface precommit event
    This is unused.

--- types/wlr_compositor.c
+++ types/wlr_compositor.c
@@ -443,8 +443,6 @@ static void surface_commit_state(struct wlr_surface *surface,
 		struct wlr_surface_state *next) {
 	assert(next->cached_state_locks == 0);
 
-	wl_signal_emit_mutable(&surface->events.precommit, next);
-
 	bool invalid_buffer = next->committed & WLR_SURFACE_STATE_BUFFER;
 
 	if (invalid_buffer && next->buffer == NULL) {
@@ -709,7 +707,6 @@ static struct wlr_surface *surface_create(struct wl_client *client,
 	surface->pending.seq = 1;
 
 	wl_signal_init(&surface->events.client_commit);
-	wl_signal_init(&surface->events.precommit);
 	wl_signal_init(&surface->events.commit);
 	wl_signal_init(&surface->events.map);
 	wl_signal_init(&surface->events.unmap);
