545c2fec46b42bca13d9bf5c8081d0571289e1ec
xurui <xurui@kylinos.cn>
2026-01-23 09:47:10 +0800

    fix(xwm): read all properties at map requst
    Bug: #482259
    
    Signed-off-by: xurui <xurui@kylinos.cn>

--- xwayland/xwm.c
+++ xwayland/xwm.c
@@ -984,27 +984,7 @@ static const struct wlr_addon_interface surface_addon_impl = {
 	.destroy = xwayland_surface_handle_addon_destroy,
 };
 
-static void xwayland_surface_associate(struct wlr_xwm *xwm,
-		struct wlr_xwayland_surface *xsurface, struct wlr_surface *surface) {
-	assert(xsurface->surface == NULL);
-
-	wl_list_remove(&xsurface->unpaired_link);
-	wl_list_init(&xsurface->unpaired_link);
-	xsurface->surface_id = 0;
-
-	xsurface->surface = surface;
-	wlr_addon_init(&xsurface->surface_addon, &surface->addons, NULL, &surface_addon_impl);
-
-	xsurface->surface_commit.notify = xwayland_surface_handle_commit;
-	wl_signal_add(&surface->events.commit, &xsurface->surface_commit);
-
-	xsurface->surface_map.notify = xwayland_surface_handle_map;
-	wl_signal_add(&surface->events.map, &xsurface->surface_map);
-
-	xsurface->surface_unmap.notify = xwayland_surface_handle_unmap;
-	wl_signal_add(&surface->events.unmap, &xsurface->surface_unmap);
-
-	// read all surface properties
+static void read_all_surface_properties(struct wlr_xwm *xwm, struct wlr_xwayland_surface *xsurface) {
 	const xcb_atom_t props[] = {
 		XCB_ATOM_WM_CLASS,
 		XCB_ATOM_WM_NAME,
@@ -1036,6 +1016,29 @@ static void xwayland_surface_associate(struct wlr_xwm *xwm,
 		read_surface_property(xwm, xsurface, props[i], reply);
 		free(reply);
 	}
+}
+
+static void xwayland_surface_associate(struct wlr_xwm *xwm,
+		struct wlr_xwayland_surface *xsurface, struct wlr_surface *surface) {
+	assert(xsurface->surface == NULL);
+
+	wl_list_remove(&xsurface->unpaired_link);
+	wl_list_init(&xsurface->unpaired_link);
+	xsurface->surface_id = 0;
+
+	xsurface->surface = surface;
+	wlr_addon_init(&xsurface->surface_addon, &surface->addons, NULL, &surface_addon_impl);
+
+	xsurface->surface_commit.notify = xwayland_surface_handle_commit;
+	wl_signal_add(&surface->events.commit, &xsurface->surface_commit);
+
+	xsurface->surface_map.notify = xwayland_surface_handle_map;
+	wl_signal_add(&surface->events.map, &xsurface->surface_map);
+
+	xsurface->surface_unmap.notify = xwayland_surface_handle_unmap;
+	wl_signal_add(&surface->events.unmap, &xsurface->surface_unmap);
+
+	read_all_surface_properties(xwm, xsurface);
 
 	wl_signal_emit_mutable(&xsurface->events.associate, NULL);
 }
@@ -1188,6 +1191,7 @@ static void xwm_handle_map_request(struct wlr_xwm *xwm,
 		return;
 	}
 
+	read_all_surface_properties(xwm, xsurface);
 	wlr_xwayland_surface_set_withdrawn(xsurface, false);
 	wlr_xwayland_surface_restack(xsurface, NULL, XCB_STACK_MODE_BELOW);
 	xcb_map_window(xwm->xcb_conn, ev->window);
